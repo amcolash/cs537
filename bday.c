fa78304af41b55615f4fe01567bb452514f9c161
Andrew McOlash <amcolash@gmail.com>
2014-09-02 21:22:45 -0700

    Worked on detecting duplicates in array

--- bday.c
+++ bday.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define TRIALS 5
+
 int main() {
 
   /* Declare variables */
   FILE * inFile;
   FILE * outFile;
   char buffer [100];
-  int randNum;
   int sampleSize;
 
   /* Initialize files */
@@ -29,14 +30,41 @@ int main() {
     /* Loop through input file */
     while ( fgets(buffer, 100, inFile) != NULL ) {
       sampleSize = atoi(buffer);
-      printf("========================================\nSamples: %i\n", sampleSize);
-      int x;
-      for (x=0; x < sampleSize; x++) {
-        randNum = rand() % 365 + 1;
-        printf("%i, ", randNum);
+      if (sampleSize > 0) {
+        printf("======================================================================\nSamples: %i, Trials: %i\n", sampleSize, TRIALS);
+        int x;
+        int y;
+        int z;
+
+        /* Perform the requested number of trials */
+        for (x=0; x < TRIALS; x++) {
+
+          printf("\nTrial [%i]: ", x);
+
+          // int randNumber[sampleSize];
+          int randNumber[5] = {1,1,2,2,3};
+          // /* Generate the random sample data and add to an array */
+          for (y=0; y < sampleSize; y++) {
+          //   randNumber[y] = rand() % 365 + 1;
+            printf("%i ", randNumber[y]);
+          }
+
+          /* Now loop through and check to see if duplicate items exist */
+          int dupNumber[sampleSize];
+          int dupFound;
+          for (y=0; y < sampleSize; y++) {
+            for (z=0; z < sampleSize; z++) {
+              if (randNumber[y] == randNumber[z]) {
+                dupNumber[y]++;                
+              }
+            }
+            printf("\nNumber of (%i): %i", randNumber[y], dupNumber[y]);
+          }
+
+        }
+        printf("\n");
+        //fputs(buffer, outFile);
       }
-      printf("\n");
-      fputs(buffer, outFile);
     }
 
   }
