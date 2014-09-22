65ee99bc4faf01f8ee461183337ee90eb8f61ee0
Andrew McOlash <amcolash@gmail.com>
2014-09-21 22:15:08 -0500

    Parsing of parameters done using strtok

--- mysh.c
+++ mysh.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
+#include <unistd.h>
+
+void parseCmd(char * buffer) {
+  char * temp;
+  int count;
+
+  count = 0;
+  temp = strtok(buffer," \n");
+
+  while (temp != NULL) {
+    printf("[%d]: %s\n",count, temp);
+    temp = strtok(NULL, " \n");
+    count++;
+  }
+}
 
 int main(int argc, char *argv[]) {
 
@@ -9,7 +24,18 @@ int main(int argc, char *argv[]) {
 
   while (strcmp(buffer, "exit") != 0) {
     printf("mysh> ");
-    scanf("%s", buffer);
+    fgets(buffer, 1024, stdin);
+    parseCmd(buffer);
+
+
+    if (strcmp(buffer, "test") == 0) {
+      printf("something fancy!\n");
+    } else if (strcmp(buffer, "xxx") == 0) {
+      // do something else
+    } else {
+      // something new
+    }
+
   }
 
   exit(0);
