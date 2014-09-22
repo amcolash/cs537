cde34532639332c90202c775c1ca8a499a1ba484
Andrew McOlash <amcolash@gmail.com>
2014-09-21 23:34:25 -0500

    Args now updated back - need to change to malloc each time though

--- mysh.c
+++ mysh.c
@@ -3,35 +3,44 @@
 #include <string.h>
 #include <unistd.h>
 
-void parseCmd(char * buffer) {
+#define MAXARGS 20
+
+int parseCmd(char * buffer, char * args[]) {
   char * temp;
   int count;
 
   count = 0;
   temp = strtok(buffer," \n");
 
-  while (temp != NULL) {
+  while (temp != NULL && count < MAXARGS - 1) {
     printf("[%d]: %s\n",count, temp);
+    args[count] = temp;
     temp = strtok(NULL, " \n");
     count++;
   }
+  args[count] = NULL;
+
+  return count;
 }
 
 int main(int argc, char *argv[]) {
 
   /* Declare variables */
   char buffer [1024];
+  char * args[MAXARGS];
+  int argCount;
 
   while (strcmp(buffer, "exit") != 0) {
     printf("mysh> ");
     fgets(buffer, 1024, stdin);
-    parseCmd(buffer);
+    argCount = parseCmd(buffer, args);
+
+    char* arg[] = {"ls", "-l", NULL};
+    execvp(arg[0],arg);
 
 
     if (strcmp(buffer, "test") == 0) {
       printf("something fancy!\n");
-    } else if (strcmp(buffer, "xxx") == 0) {
-      // do something else
     } else {
       // something new
     }
